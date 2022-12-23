2b3d441c633c9634ae3a51f80de9a5fd12fbd8e8
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2022-12-23 20:03:19 +0500

    Django ORMs

---
+++ storefront/playground/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from store.models import Product
+
+
+def say_hello(request):
+    queryset = Product.objects.filter(description__isnull=True)
+
+    return render(request, 'hello.html', {'name': 'Waqar','products':list(queryset)})
