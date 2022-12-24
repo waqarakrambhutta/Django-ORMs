85e11b163955e059e5722a937a7a722f9e391f61
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2022-12-24 21:55:47 +0500

    F objects.

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.db.models import Q
+from django.db.models import Q, F
 from store.models import Product
 
 
 def say_hello(request):
-    queryset = Product.objects.filter(~Q(inventory__lt=10))
+    queryset = Product.objects.filter(inventory=F('unit_price'))
 
     return render(request, 'hello.html', {'name': 'Waqar','products':list(queryset)})
