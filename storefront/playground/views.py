63e2498671808973e33af4dbd2a0f0e694ffad6c
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2022-12-24 20:10:21 +0500

    AND, OR , NOT in ORMs.

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
 from store.models import Product
 
 
 def say_hello(request):
-    queryset = Product.objects.filter(description__isnull=True)
+    queryset = Product.objects.filter(~Q(inventory__lt=10))
 
     return render(request, 'hello.html', {'name': 'Waqar','products':list(queryset)})
