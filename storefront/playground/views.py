b8a23c9424652ca0554962d59b7f1046ac619898
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2022-12-25 20:39:09 +0500

    Selecting fields in the query.

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Q, F
-from store.models import Customer
+from store.models import Product
 
 
 def say_hello(request):
-    queryset = Customer.objects.earliest('last_name')
+    queryset = Product.objects.values_list('id','title','collection__title')
     
-
     return render(request, 'hello.html', {'name': 'Waqar','products':list(queryset)})
-    
-    
-# from django.shortcuts import render
-# from django.http import HttpResponse
-# from django.db.models import Q, F
-# from store.models import Product
-
-
-# def say_hello(request):
-#     queryset = Product.objects.filter(customer__startswith='F')
-    
-
-#     return render(request, 'hello.html', {'name': 'Waqar','products':list(queryset)})
