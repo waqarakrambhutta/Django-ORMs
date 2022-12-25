fc6ffc211b0042f0267af28e9a2f94572cf83a8e
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2022-12-25 13:16:50 +0500

    practice of Django ORMs

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Q, F
-from store.models import Product
+from store.models import Customer
 
 
 def say_hello(request):
-    queryset = Product.objects.earliest('unit_price')
+    queryset = Customer.objects.earliest('last_name')
     
 
     return render(request, 'hello.html', {'name': 'Waqar','products':list(queryset)})
+    
+    
+# from django.shortcuts import render
+# from django.http import HttpResponse
+# from django.db.models import Q, F
+# from store.models import Product
+
+
+# def say_hello(request):
+#     queryset = Product.objects.filter(customer__startswith='F')
+    
+
+#     return render(request, 'hello.html', {'name': 'Waqar','products':list(queryset)})
