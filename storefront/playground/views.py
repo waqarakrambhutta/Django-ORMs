28cfb2e78855081d9c44e813f0566275dc538a10
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2022-12-26 15:40:02 +0500

    selecting related objects

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -4,7 +4,8 @@ from django.db.models import Q, F
 from store.models import Product
 
 
+
 def say_hello(request):
-    queryset = Product.objects.select_related('collection__someOtherField').all()
+    queryset = Product.objects.prefetch_related('promotions').all()
     
     return render(request, 'hello.html', {'name': 'Waqar','products':list(queryset)})
