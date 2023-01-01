edd8b632d93a94fa4e6e95fe284b2bf87eb14870
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-01-01 19:49:02 +0500

    Django ORMs

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -6,7 +6,11 @@ from store.models import Product,Order
 
 
 def say_hello(request):
-    queryset = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]    
+    queryset = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:10]    
     return render(request, 'hello.html', {'name': 'Waqar','products':list(queryset)})
+    
+# def say_hello(request):
+#     queryset = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]    
+#     return render(request, 'hello.html', {'name': 'Waqar','products':list(queryset)})
     
     
