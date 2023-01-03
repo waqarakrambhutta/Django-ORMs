857ab528c5537e61b47328c354dd91631a3f89e8
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-01-03 18:07:49 +0500

    my streek

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -7,12 +7,9 @@ from store.models import Product,Order
 
 def say_hello(request):
     queryset = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:10] 
-    # if we write minus with the field name itmeans it is sorted in descending order.
-    # or if there's not any sign than it means it is sorted in the ascending order.
+    
     return render(request, 'hello.html', {'name': 'Waqar','products':list(queryset)})
     
-# def say_hello(request):
-#     queryset = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]    
-#     return render(request, 'hello.html', {'name': 'Waqar','products':list(queryset)})
+
     
     
