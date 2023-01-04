99dfebeca176d97ee98c1d0cc2e04e50b3797dbd
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-01-04 18:38:18 +0500

    Aggregation objects.

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.db.models import Q, F
-from store.models import Product,Order
+from django.db.models.aggregates import Count,Max
+from store.models import Product
 
 
 
 def say_hello(request):
-    queryset = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:10] 
+    result=Product.objects.aggregate(Max('unit_price'))
     
-    return render(request, 'hello.html', {'name': 'Waqar','products':list(queryset)})
+    return render(request, 'hello.html', {'name': 'Waqar','result':result})
     
 
     
