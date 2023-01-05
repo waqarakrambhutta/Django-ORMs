139c62ba5e4ad0b39d65b8d7e797b72ac74e0c02
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-01-05 22:17:52 +0500

    For streek

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.db.models.aggregates import Count,Max
+from django.db.models.aggregates import Count,Max,Min
 from store.models import Product
 
 
 
 def say_hello(request):
-    result=Product.objects.aggregate(Max('unit_price'))
+    result=Product.objects.aggregate(Min('unit_price'))
     
     return render(request, 'hello.html', {'name': 'Waqar','result':result})
     
