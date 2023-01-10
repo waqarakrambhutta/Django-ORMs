0eb0613934e3dc5f8f00d3bcc14e296fd5079167
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-01-10 22:23:41 +0500

    For streek

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -6,7 +6,7 @@ from store.models import Product
 
 
 def say_hello(request):
-    result=Product.objects.filter(unit_price__gt=1.3).aggregate(Count('unit_price'))
+    result=Product.objects.filter(unit_price__lt=1).aggregate(Count('unit_price'))
     
     return render(request, 'hello.html', {'name': 'Waqar','result':result})
     
