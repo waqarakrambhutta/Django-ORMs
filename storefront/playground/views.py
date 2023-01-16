16cdc20a9d38350483735d153569bcad665b16e7
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-01-16 21:38:40 +0500

    calling database functions.

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models.aggregates import Count,Max
-from store.models import Product
-from django.db.models import Value,F
+from store.models import Product,Customer
+from django.db.models import Value,F,Func
+from django.db.models.functions import Concat
 
 
 
 def say_hello(request):
-    result=Product.objects.annotate(tax=F('unit_price')+3)
+    result=Customer.objects.annotate(
+        Concat=('first_name',Value(' '),'last_name')
+    )
     
     return render(request, 'hello.html', {'name': 'Waqar','result':list(result)})
