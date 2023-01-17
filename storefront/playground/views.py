2dbab83d01db1ceaf7dafa14d703a63973affdbf
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-01-17 12:56:35 +0500

    Grouping data

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.db.models.aggregates import Count,Max
 from store.models import Product,Customer
-from django.db.models import Value,F,Func
-from django.db.models.functions import Concat
+from django.db.models import Value,F,Func,Count
+
 
 
 
 def say_hello(request):
     result=Customer.objects.annotate(
-        Concat=('first_name',Value(' '),'last_name')
+        order_count=Count('order')
     )
     
     return render(request, 'hello.html', {'name': 'Waqar','result':list(result)})
