200bfb4735eb4816069a8cf1cd18b84c822d0067
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-01-17 15:10:33 +0500

    Expressionwrappers

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from store.models import Product,Customer
-from django.db.models import Value,F,Func,Count
-
-
-
+from django.db.models import Value,F, ExpressionWrapper,DecimalField
 
 def say_hello(request):
-    result=Customer.objects.annotate(
-        order_count=Count('order')
-    )
+    discounted_price = ExpressionWrapper(F('unit_price') * 0.8,output_field=DecimalField())
+    result=Product.objects.annotate(
+    discount= discounted_price 
+        )
     
     return render(request, 'hello.html', {'name': 'Waqar','result':list(result)})
