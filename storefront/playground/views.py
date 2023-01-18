1f93ef1b3c3024b10e6076cdd6b95597ad183c84
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-01-18 12:02:08 +0500

    querying generic relationships

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-from store.models import Product,Customer
-from django.db.models import Value,F, ExpressionWrapper,DecimalField
+from store.models import Product
+from django.contrib.contenttypes.models import ContentType
+from tags.models import TaggedItem
 
 def say_hello(request):
-    discounted_price = ExpressionWrapper(F('unit_price') * 0.8,output_field=DecimalField())
-    result=Product.objects.annotate(
-    discount= discounted_price 
-        )
-    
-    return render(request, 'hello.html', {'name': 'Waqar','result':list(result)})
+    content_type=ContentType.objects.get_for_model(Product)
+    queryset = TaggedItem.objects \
+    .select_related('tag') \
+    .filter(
+        content_type=content_type,
+        object_id=1
+    )
+    return render(request, 'hello.html', {'name': 'Waqar','tags':list(queryset)})
+
