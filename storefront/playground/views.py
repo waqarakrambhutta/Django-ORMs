78b23b8c683ae28c709056a660d8f4f1a36e4006
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-01-18 17:34:31 +0500

    understanding querying cache

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from store.models import Product
 from django.contrib.contenttypes.models import ContentType
-from tags.models import TaggedItem
+
 
 def say_hello(request):
-    queryset=TaggedItem.objects.get_tags_for(Product, 1)
-
-
-    return render(request, 'hello.html', {'name': 'Waqar','tags':list(queryset)})
+    
+    queryset=Product.objects.all()
+    queryset[0]
+    list(queryset)
+    
+    return render(request, 'hello.html', {'name': 'Waqar'})
 
