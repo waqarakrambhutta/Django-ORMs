b857cd14230247ff6cf27c6a74eb2afb4873eac4
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-01-18 18:11:43 +0500

    creating object

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from store.models import Product
 from django.contrib.contenttypes.models import ContentType
-
+from store.models import Product,Collection
 
 def say_hello(request):
     
-    queryset=Product.objects.all()
-    queryset[0]
-    list(queryset)
-    
+    collection = Collection()
+    collection.title= 'Video games.'
+    # collection.featured_product = Product(pk=1)
+    collection.featured_product_id = 1
+    collection.save()
+    collection.id
+
+    # this is shorter approch but i'll not prefer it.
+    # collection= Collection.objects.create(title='video game',featured_product=1)
+    # collection.id
+
     return render(request, 'hello.html', {'name': 'Waqar'})
 
