5ff1b175c43e07a7dfa0425b80878bcc1a8c20fd
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-01-18 21:12:27 +0500

    updating objects

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from store.models import Product
 from django.contrib.contenttypes.models import ContentType
-from tags.models import Tag
+from store.models import Product,Collection
 
 def say_hello(request):
-    # tag=Tag()
-    # tag.label='this is first tag.'
-    # tag.save()
-    # tag.id
 
-    Tag.objects.create(label='this is my second tag')
+    # collection = Collection.objects.get(pk=11)
+    # collection.featured_product=None
+    # collection.save()
+    # collection.id
+
+    Collection.objects.filter(pk=11).update(featured_product=None)
     
-
     return render(request, 'hello.html', {'name': 'Waqar'})
 
