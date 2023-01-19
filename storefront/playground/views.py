26529cf9dc31f946637341aca201760e30f36977
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-01-19 13:12:57 +0500

    deleting objects

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
 from store.models import Product
 from django.contrib.contenttypes.models import ContentType
-from store.models import Product,Collection
+from store.models import Collection
 
 def say_hello(request):
 
-    # collection = Collection.objects.get(pk=11)
-    # collection.featured_product=None
-    # collection.save()
-    # collection.id
+    # collection = Collection(pk=11)
+    # collection.delete()
 
-    Collection.objects.filter(pk=11).update(featured_product=None)
-    
+    Collection.objects.filter(pk__range=(14,24)).delete()
+
     return render(request, 'hello.html', {'name': 'Waqar'})
 
